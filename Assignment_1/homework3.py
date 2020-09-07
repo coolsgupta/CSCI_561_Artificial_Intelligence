c5b2564281b9516365cd501a384ac131126bd08f
Sachin Gupta <cool.sgupta@gmail.com>
2020-09-06 18:32:31 -0700

    inheriting bfs from pathfinder; created function to get reachable points

--- Assignment_1/homework3.py
+++ Assignment_1/homework3.py
@@ -2,6 +2,28 @@ import numpy as np
 
 
 class Utils:
+
+    actions_map = {
+        1 :  (1,0,0),
+        2 :  (-1,0,0),
+        3 :  (0,1,0),
+        4 :  (0,-1,0),
+        5 :  (0,0,1),
+        6 :  (0,0,-1),
+        7 :  (1,1,0),
+        8 :  (1,-1,0),
+        9 :  (-1,1,0),
+        10 : (-1,-1,0),
+        11 : (1,0,1),
+        12 : (1,0,-1),
+        13 : (-1,0,1),
+        14 : (-1,0,-1),
+        15 : (0,1,1),
+        16 : (0,1,-1),
+        17 : (0,-1,1),
+        18 : (0,-1,-1),
+    }
+
     @staticmethod
     def read_file(input_path):
         with open(input_path) as input_file:
@@ -13,6 +35,14 @@ class Utils:
     @staticmethod
     def write_file():
         return
+
+    @staticmethod
+    def cal_euclidean_distance(point_1, point_2):
+        return sum([(x-y)**2 for x,y in zip(point_1, point_2)])**0.5
+
+    @staticmethod
+    def add_action_step(current_point, action):
+        return tuple(map(lambda i, j: i + j, current_point, Utils.actions_map[action]))
 
 
 class PathFinder:
@@ -26,6 +56,20 @@ class PathFinder:
 
     def get_action_point_action_map(self, action_point_action_list):
         return {tuple(x[:3]): x[3:] for x in action_point_action_list}
+
+
+class BFSPathFinder(PathFinder):
+    def __init__(self, data):
+        super(BFSPathFinder, self).__init__(data)
+
+    def find_reachable_points(self, current_point, allowed_actions):
+        reachable_points_from_action = {}
+        for action in allowed_actions:
+            next_state = Utils.add_action_step(current_point, action)
+            if next_state in self.action_points:
+                reachable_points_from_action[next_state] = action
+
+        return reachable_points_from_action
 
 
 if __name__ == '__main__':
