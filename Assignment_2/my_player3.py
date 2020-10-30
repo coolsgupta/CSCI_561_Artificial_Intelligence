6bec3b23255a642039e931b75594fa4f00b27534
Sachin Gupta <cool.sgupta@gmail.com>
2020-10-30 14:15:18 -0700

    submission: 28: code clarity

--- Assignment_2/my_player3.py
+++ Assignment_2/my_player3.py
@@ -8,48 +8,50 @@ class Agent:
         self.my_player = my_player
         self.depth = 4
         self.game_host = GameHost(previous_board=previous_board, current_board=current_board, player=my_player)
-        self.BLACK_PIECES = 0
-        self.WHITE_PIECES = 0
+        self.board_size = 5
+        self.komi = 2.5
+        self.score_black = 0
+        self.score_white = 0
 
     def heuristic_score(self, board, player, died_pieces_black, died_pieces_white):
-        black_count = 0
-        white_count = 0
+        num_black_pieces = 0
+        num_white_pieces = 0
         black_endangered_liberty = 0
         white_endangered_liberty = 0
-        for i in range(0, 5):
-            for j in range(0, 5):
+        for i in range(0, self.board_size):
+            for j in range(0, self.board_size):
                 if board[i][j] == 1:
                     lib = self.game_host.get_liberty_positions((i, j), board, 1)
                     if len(lib) <= 1:
                         black_endangered_liberty = black_endangered_liberty + 1
-                    black_count += 1
+                    num_black_pieces += 1
                 elif board[i][j] == 2:
                     lib = self.game_host.get_liberty_positions((i, j), board, 2)
                     if len(lib) <= 1:
                         white_endangered_liberty = white_endangered_liberty + 1
-                    white_count += 1
-        white_count = white_count + 2.5
+                    num_white_pieces += 1
+        num_white_pieces = num_white_pieces + self.komi
         if player == 1:
-            eval_value = black_count-white_count+white_endangered_liberty-black_endangered_liberty+died_pieces_white*10-died_pieces_black*16
+            eval_value = num_black_pieces-num_white_pieces+white_endangered_liberty-black_endangered_liberty+died_pieces_white*10-died_pieces_black*16
         else:
-            eval_value = -black_count + white_count-white_endangered_liberty+black_endangered_liberty+died_pieces_black*10-died_pieces_white*16
+            eval_value = -num_black_pieces + num_white_pieces-white_endangered_liberty+black_endangered_liberty+died_pieces_black*10-died_pieces_white*16
 
         return eval_value
 
     def max_move(self, board, previous_board, player, depth, alpha, beta, new_board_without_died_pieces):
         if player == 2:
             died_pieces_white = len(self.game_host.find_died_pieces(new_board_without_died_pieces, player))
-            self.WHITE_PIECES = self.WHITE_PIECES + died_pieces_white
+            self.score_white = self.score_white + died_pieces_white
         if player == 1:
             died_pieces_black = len(self.game_host.find_died_pieces(new_board_without_died_pieces, player))
-            self.BLACK_PIECES = self.BLACK_PIECES + died_pieces_black
+            self.score_black = self.score_black + died_pieces_black
 
         if depth == 0:
-            value = self.heuristic_score(board, player, self.BLACK_PIECES, self.WHITE_PIECES)
+            value = self.heuristic_score(board, player, self.score_black, self.score_white)
             if player == 1:
-                self.BLACK_PIECES = self.BLACK_PIECES - len(self.game_host.find_died_pieces(new_board_without_died_pieces, 1))
+                self.score_black = self.score_black - len(self.game_host.find_died_pieces(new_board_without_died_pieces, 1))
             if player == 2:
-                self.WHITE_PIECES = self.WHITE_PIECES - len(self.game_host.find_died_pieces(new_board_without_died_pieces, 2))
+                self.score_white = self.score_white - len(self.game_host.find_died_pieces(new_board_without_died_pieces, 2))
             return value, []
 
         max_score = float("-inf")
@@ -80,17 +82,17 @@ class Agent:
     def min_move(self, board, previous_board, player, depth, alpha, beta, new_board_without_died_pieces):
         if player == 2:
             died_pieces_white = len(self.game_host.find_died_pieces(new_board_without_died_pieces, player))
-            self.WHITE_PIECES = self.WHITE_PIECES + died_pieces_white
+            self.score_white = self.score_white + died_pieces_white
         if player == 1:
             died_pieces_black = len(self.game_host.find_died_pieces(new_board_without_died_pieces, player))
-            self.BLACK_PIECES = self.BLACK_PIECES + died_pieces_black
+            self.score_black = self.score_black + died_pieces_black
 
         if depth == 0:
-            value = self.heuristic_score(board, player, self.BLACK_PIECES, self.WHITE_PIECES)
+            value = self.heuristic_score(board, player, self.score_black, self.score_white)
             if player == 1:
-                self.BLACK_PIECES = self.BLACK_PIECES - len(self.game_host.find_died_pieces(new_board_without_died_pieces, 1))
+                self.score_black = self.score_black - len(self.game_host.find_died_pieces(new_board_without_died_pieces, 1))
             if player == 2:
-                self.WHITE_PIECES = self.WHITE_PIECES - len(self.game_host.find_died_pieces(new_board_without_died_pieces, 2))
+                self.score_white = self.score_white - len(self.game_host.find_died_pieces(new_board_without_died_pieces, 2))
             return value, []
 
         min_score = float("inf")
@@ -118,13 +120,13 @@ class Agent:
 
     def play(self):
         score, actions = self.max_move(
-            self.current_board,
-            self.previous_board,
-            self.my_player,
-            self.depth,
-            float("-inf"),
-            float("inf"),
-            self.current_board
+            board=self.current_board,
+            previous_board=self.previous_board,
+            player=self.my_player,
+            depth=self.depth,
+            alpha=float("-inf"),
+            beta=float("inf"),
+            new_board_without_died_pieces=self.current_board
         )
         IOManager.write_move(actions[0] if len(actions) > 0 else 'PASS')
 
