1846558b688b13df2233259df09ba2a8c99c1554
Sachin Gupta <cool.sgupta@gmail.com>
2020-10-30 19:05:10 -0700

    43

--- Assignment_2/my_player3.py
+++ Assignment_2/my_player3.py
@@ -2,43 +2,57 @@ from utils import *
 
 
 class Agent:
-    def __init__(self, my_player, previous_board, current_board):
-        self.previous_board = previous_board
-        self.current_board = current_board
-        self.my_player = my_player
-        self.depth = 4
-        self.game_host = GameHost(previous_board=previous_board, current_board=current_board, player=my_player)
+    def __init__(self):
+        self.get_host()
         self.board_size = 5
+        self.depth = 4
         self.komi = 2.5
         self.score_black = 0
         self.score_white = 0
 
+    def get_host(self):
+        my_player, previous_board, current_board = IOManager.read_board()
+        self.previous_board = previous_board
+        self.current_board = current_board
+        self.my_player = my_player
+        self.game_host = GameHost(
+            previous_board=previous_board,
+            current_board=current_board,
+            player=my_player
+        )
+
     def heuristic_score(self, board, player, died_pieces_black, died_pieces_white):
-        num_black_pieces = 0
-        num_white_pieces = 0
-        black_endangered_liberty = 0
-        white_endangered_liberty = 0
+        score_black, score_white, danger_black, danger_white = 0, self.komi, 0, 0
         for i in range(self.board_size):
             for j in range(self.board_size):
                 if board[i][j] == 1:
-                    lib = self.game_host.get_liberty_positions((i, j), board, 1)
-                    if len(lib) <= 1:
-                        black_endangered_liberty = black_endangered_liberty + 1
-                    num_black_pieces += 1
+                    score_black += 1
+                    available_liberty = self.game_host.get_liberty_positions((i, j), board, 1)
+                    if len(available_liberty) <= 1:
+                        danger_black += 1
+
                 elif board[i][j] == 2:
-                    lib = self.game_host.get_liberty_positions((i, j), board, 2)
-                    if len(lib) <= 1:
-                        white_endangered_liberty = white_endangered_liberty + 1
-                    num_white_pieces += 1
-        num_white_pieces = num_white_pieces + self.komi
+                    score_white += 1
+                    available_liberty = self.game_host.get_liberty_positions((i, j), board, 2)
+                    if len(available_liberty) <= 1:
+                        danger_white += 1
+
         if player == 1:
-            eval_value = num_black_pieces-num_white_pieces+white_endangered_liberty-black_endangered_liberty+died_pieces_white*10-died_pieces_black*16
+            heuristic_score = score_black-score_white+danger_white-danger_black+died_pieces_white*10-died_pieces_black*16
         else:
-            eval_value = -num_black_pieces + num_white_pieces-white_endangered_liberty+black_endangered_liberty+died_pieces_black*10-died_pieces_white*16
+            heuristic_score = -score_black + score_white-danger_white+danger_black+died_pieces_black*10-died_pieces_white*16
 
-        return eval_value
+        return heuristic_score
 
-    def max_move(self, board, previous_board, player, depth, alpha, beta, new_board_without_died_pieces):
+    def max_move(self, **kwargs):
+        board = kwargs.get('board')
+        previous_board = kwargs.get('previous_board')
+        player = kwargs.get('player')
+        depth = kwargs.get('depth')
+        alpha = kwargs.get('alpha')
+        beta = kwargs.get('beta')
+        new_board_without_died_pieces = kwargs.get('new_board_without_died_pieces')
+
         if player == 2:
             died_pieces_white = len(self.game_host.find_died_pieces(new_board_without_died_pieces, player))
             self.score_white = self.score_white + died_pieces_white
@@ -64,7 +78,13 @@ class Agent:
             trial_board = deepcopy(board)
             next_board, died_pieces, new_board_without_died_pieces = self.game_host.check_move_validity_score((move[0], move[1]), trial_board, player)
             score, actions = self.min_move(
-                next_board, board, 3 - player, depth - 1, alpha, beta, new_board_without_died_pieces
+                board=next_board,
+                previous_board=board,
+                player=3 - player,
+                depth=depth - 1,
+                alpha=alpha,
+                beta=beta,
+                new_board_without_died_pieces=new_board_without_died_pieces
             )
 
             if score > max_score:
@@ -79,7 +99,15 @@ class Agent:
 
         return max_score, max_score_actions
 
-    def min_move(self, board, previous_board, player, depth, alpha, beta, new_board_without_died_pieces):
+    def min_move(self, **kwargs):
+        board = kwargs.get('board')
+        previous_board = kwargs.get('previous_board')
+        player = kwargs.get('player')
+        depth = kwargs.get('depth')
+        alpha = kwargs.get('alpha')
+        beta = kwargs.get('beta')
+        new_board_without_died_pieces = kwargs.get('new_board_without_died_pieces')
+
         if player == 2:
             died_pieces_white = len(self.game_host.find_died_pieces(new_board_without_died_pieces, player))
             self.score_white = self.score_white + died_pieces_white
@@ -103,8 +131,17 @@ class Agent:
             trial_board = deepcopy(board)
             next_board, died_pieces, new_board_without_died_pieces = self.game_host.check_move_validity_score((move[0], move[1]), trial_board, player)
             score, actions = self.max_move(
-                next_board, board, 3 - player, depth - 1, alpha, beta, new_board_without_died_pieces
+                board=next_board,
+                previous_board=board,
+                player=3 - player,
+                depth=depth - 1,
+                alpha=alpha,
+                beta=beta,
+                new_board_without_died_pieces=new_board_without_died_pieces
             )
+            #     self.max_move(
+            #     next_board, board, 3 - player, depth - 1, alpha, beta, new_board_without_died_pieces
+            # )
 
             if score < min_score:
                 min_score = score
@@ -132,5 +169,4 @@ class Agent:
 
 
 if __name__ == '__main__':
-    player, previous_board, board = IOManager.read_board()
-    Agent(player, previous_board, board).play()
+    Agent().play()
